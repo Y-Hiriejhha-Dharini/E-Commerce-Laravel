71f92724fb5a8cb56debe52df89e3b4b020200e3
Y-Hiriejhha-Dharini <hirijhaa@gmail.com>
2021-10-16 23:03:02 +0530

    login done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Harini',
+            'email'=>'harini@gmail.com',
+            'email_verified_at' => '2021-10-16',
+            'password'=>Hash::make('12345'),
+            'created_at' => '2021-10-16',
+            'updated_at' => '2021-10-16'
+        ]);
+    }
+}
